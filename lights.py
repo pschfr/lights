d17978d1f31e00822ff2e594323ed673c92fb1d1
Paul Schaefer <yeppaul2@gmail.com>
2017-01-03 20:39:14 -0500

    added comments and cleaned up code

--- lights.py
+++ lights.py
@@ -1,28 +1,13 @@
 #!/usr/bin/python
-# pylint: disable=C0103
 """This module controls my Philips Hue lights."""
 
-import logging
-from phue import Bridge
+import logging           # import logging
+from phue import Bridge  # import Bridge from phue.py
 
-logging.basicConfig()
-br = Bridge('10.0.0.215')
+logging.basicConfig()    # initates console logging
+B = Bridge('10.0.0.215') # connects to the philips hue bridge
 
-# this only needs to be run once
-# br.connect()
-
-lights = br.lights
-
-# print lights[0].name
-# lights[0].on = True
-# lights[0].brightness = 255
-
-# print lights[1].name
-# lights[1].on = True
-# lights[1].brightness = 255
-
-# Loop through lights, turning them on
-for l in lights:
-    print l.name
-    l.on = True
-    l.brightness = 255
+for L in B.lights:       # loops through the lights
+    print L.name         # prints the name
+    L.on = True          # gotta turn it on first
+    L.brightness = 255   # 0-255
