41a31d9fa3e0e5ec41d75541a9a5bab1e2bd2362
Paul Schaefer <yeppaul2@gmail.com>
2017-01-07 17:11:19 -0500

    updated script to prompt for which lights, and brightness; added screenshot

--- lights.py
+++ lights.py
@@ -5,9 +5,36 @@ import logging           # import logging
 from phue import Bridge  # import Bridge from phue.py
 
 logging.basicConfig()    # initates console logging
-B = Bridge('10.0.0.215') # connects to the philips hue bridge
+BRIDGE = Bridge('10.0.0.215') # connects to the philips hue bridge
 
-for L in B.lights:       # loops through the lights
-    print L.name         # prints the name
-    L.on = True          # gotta turn it on first
-    L.brightness = 255   # 0-255
+print 'Connected to Philips Hue Bridge\n'
+
+WHICH = raw_input('Which lights? (Both, Landing, or Bedroom) ')
+STATE = raw_input('On or off? ')
+
+if STATE == 'on': # if on, ask for brightness
+    BRIGHTNESS = int(raw_input('Brightness? (1-255) '))
+
+    if WHICH == 'both':
+        for LIGHT in BRIDGE.lights:
+            LIGHT.on = True
+            LIGHT.brightness = BRIGHTNESS
+
+    elif WHICH == 'bedroom':
+        BRIDGE.lights[0].on = True
+        BRIDGE.lights[0].brightness = BRIGHTNESS
+
+    elif WHICH == 'landing':
+        BRIDGE.lights[1].on = True
+        BRIDGE.lights[1].brightness = BRIGHTNESS
+
+elif STATE == 'off':
+    if WHICH == 'both':
+        for LIGHT in BRIDGE.lights:
+            LIGHT.on = False
+
+    elif WHICH == 'bedroom':
+        BRIDGE.lights[0].on = False
+
+    elif WHICH == 'landing':
+        BRIDGE.lights[1].on = False
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 #lights
 A small Python script to control my Philips Hue lights using [studioimaginaire/phue](https://github.com/studioimaginaire/phue)
+![](screenshot.png)
