fac51b1ba96f61d382ada8d0d72d42fadad49914
Paul Schaefer <yeppaul2@gmail.com>
2017-01-07 19:10:40 -0500

    improved input logic, I can type b instead of bedroom

--- lights.py
+++ lights.py
@@ -12,29 +12,31 @@ print 'Connected to Philips Hue Bridge\n'
 WHICH = raw_input('Which lights? (Both, Landing, or Bedroom) ')
 STATE = raw_input('On or off? ')
 
-if STATE == 'on': # if on, ask for brightness
-    BRIGHTNESS = int(raw_input('Brightness? (1-255) '))
+if STATE == 'on' or STATE == '1' or STATE == '': # if on, ask for brightness
+    BRIGHTNESS = raw_input('Brightness? (1-255) ')
+    if BRIGHTNESS == '':
+        BRIGHTNESS = 255
 
-    if WHICH == 'both':
+    if WHICH == 'both' or WHICH == '2' or WHICH == '':
         for LIGHT in BRIDGE.lights:
             LIGHT.on = True
-            LIGHT.brightness = BRIGHTNESS
+            LIGHT.brightness = int(BRIGHTNESS)
 
-    elif WHICH == 'bedroom':
+    elif WHICH == 'bedroom' or WHICH == 'bed' or WHICH == 'b' or WHICH == '0':
         BRIDGE.lights[0].on = True
-        BRIDGE.lights[0].brightness = BRIGHTNESS
+        BRIDGE.lights[0].brightness = int(BRIGHTNESS)
 
-    elif WHICH == 'landing':
+    elif WHICH == 'landing' or WHICH == 'l' or WHICH == '1':
         BRIDGE.lights[1].on = True
-        BRIDGE.lights[1].brightness = BRIGHTNESS
+        BRIDGE.lights[1].brightness = int(BRIGHTNESS)
 
-elif STATE == 'off':
-    if WHICH == 'both':
+elif STATE == 'off' or STATE == '0':
+    if WHICH == 'both' or WHICH == '2' or WHICH == '':
         for LIGHT in BRIDGE.lights:
             LIGHT.on = False
 
-    elif WHICH == 'bedroom':
+    elif WHICH == 'bedroom' or WHICH == 'bed' or WHICH == 'b' or WHICH == '0':
         BRIDGE.lights[0].on = False
 
-    elif WHICH == 'landing':
+    elif WHICH == 'landing' or WHICH == 'l' or WHICH == '1':
         BRIDGE.lights[1].on = False
