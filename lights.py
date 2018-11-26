0546d9e5786a745c666a1fd7c7a0a907cbaa1afe
Paul Schaefer <yeppaul2@gmail.com>
2018-11-25 20:09:52 -0500

    Renamed lights and fixed a typo.

--- lights.py
+++ lights.py
@@ -5,7 +5,7 @@ import sys               # import system functions
 import logging           # import logging
 from phue import Bridge  # import Bridge from phue.py
 
-logging.basicConfig()    # initates console logging
+logging.basicConfig()    # initiates console logging
 BRIDGE = Bridge('10.0.0.215') # connects to the philips hue bridge
 
 # If no arguments, print intro
@@ -16,14 +16,14 @@ if len(sys.argv) == 1:
     if BRIDGE.get_light(1, 'on') is False and BRIDGE.get_light(2, 'on') is False:
         print 'None of your lights are on.\n'
     elif BRIDGE.get_light(1, 'on') and BRIDGE.get_light(2, 'on') is False:
-        print 'Your bedroom light is on.\n'
+        print 'Your seahorse light is on.\n'
     elif BRIDGE.get_light(1, 'on') is False and BRIDGE.get_light(2, 'on'):
-        print 'Your landing light is on.\n'
+        print 'Your bedroom light is on.\n'
     elif BRIDGE.get_light(1, 'on') and BRIDGE.get_light(2, 'on'):
         print 'All your lights are on.\n'
 
     # Ask for lights to modify and how
-    WHICH = raw_input('\033[1mWhich lights? (Both, Landing, or Bedroom) \033[0m')
+    WHICH = raw_input('\033[1mWhich lights? (Both, Bedroom, or Seahorse) \033[0m')
     STATE = raw_input('\033[1mOn or off? \033[0m')
 elif len(sys.argv) > 1: # if it has arguments, set them
     WHICH = sys.argv[1]
@@ -45,11 +45,11 @@ if STATE == 'on' or STATE == '1' or STATE == '': # if on, ask for brightness
         BRIDGE.set_light([1, 2], 'on', True)
         BRIDGE.set_light([1, 2], 'bri', int(BRIGHTNESS))
 
-    elif WHICH == 'bedroom' or WHICH == 'bed' or WHICH == 'b' or WHICH == '1':
+    elif WHICH == 'seahorse' or WHICH == 'sea' or WHICH == 's' or WHICH == '1':
         BRIDGE.set_light(1, 'on', True)
         BRIDGE.set_light(1, 'bri', int(BRIGHTNESS))
 
-    elif WHICH == 'landing' or WHICH == 'l' or WHICH == '2':
+    elif WHICH == 'bedroom' or WHICH == 'bed' or WHICH == 'b' or WHICH == '2':
         BRIDGE.set_light(2, 'on', True)
         BRIDGE.set_light(2, 'bri', int(BRIGHTNESS))
 
@@ -57,8 +57,8 @@ elif STATE == 'off' or STATE == '0':
     if WHICH == 'both' or WHICH == '3' or WHICH == '':
         BRIDGE.set_light([1, 2], 'on', False)
 
-    elif WHICH == 'bedroom' or WHICH == 'bed' or WHICH == 'b' or WHICH == '0':
+    elif WHICH == 'seahorse' or WHICH == 'sea' or WHICH == 's' or WHICH == '0':
         BRIDGE.set_light(1, 'on', False)
 
-    elif WHICH == 'landing' or WHICH == 'l' or WHICH == '1':
+    elif WHICH == 'landing' or WHICH == 'bed' or WHICH == 'b' or WHICH == '1':
         BRIDGE.set_light(2, 'on', False)
